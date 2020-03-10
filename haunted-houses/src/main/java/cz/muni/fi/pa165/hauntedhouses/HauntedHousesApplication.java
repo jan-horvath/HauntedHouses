1554227b7828841739fb921b2da49750d3f99499
Jan Horvath <xhorvat6@nymfe42.fi.muni.cz>
2020-03-10 11:44:56 +0100

    Initial commit

---
+++ haunted-houses/src/main/java/cz/muni/fi/pa165/hauntedhouses/HauntedHousesApplication.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.hauntedhouses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HauntedHousesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HauntedHousesApplication.class, args);
+	}
+
+}
---
+++ haunted-houses/src/test/java/cz/muni/fi/pa165/hauntedhouses/HauntedHousesApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.hauntedhouses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HauntedHousesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
