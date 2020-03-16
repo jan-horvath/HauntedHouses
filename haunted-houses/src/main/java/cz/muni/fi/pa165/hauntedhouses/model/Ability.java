02105917eed182cf028e61290bc2eef2e8efe59a
davidhofman <davidhofman1996@gmail.com>
2020-03-16 19:59:46 +0100

    implemented Ability class

--- haunted-houses/src/main/java/cz/muni/fi/pa165/hauntedhouses/model/Ability.java
+++ haunted-houses/src/main/java/cz/muni/fi/pa165/hauntedhouses/model/Ability.java
@@ -1,4 +1,60 @@
 package cz.muni.fi.pa165.hauntedhouses.model;
 
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.util.Objects;
+
+/**
+ * @author David Hofman
+ */
+@Entity
 public class Ability {
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable=false,unique=true)
+    private String name;
+
+    @Column(nullable=false)
+    private String description;
+
+    public Ability() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj == null || this.getClass() != obj.getClass()) return false;
+        if(this == obj) return true;
+        Ability ability = (Ability) obj;
+        return name.equals(ability.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
