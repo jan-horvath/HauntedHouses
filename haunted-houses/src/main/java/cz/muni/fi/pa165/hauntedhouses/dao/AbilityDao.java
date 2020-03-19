18c1cb120d6cfa2baa1c909546c40f167c7365b9
davidhofman <47823840+davidhofman@users.noreply.github.com>
2020-03-19 13:50:05 +0100

    Implemented Ability, AbilityDao, AbilityDaoImpl (#7)
    * implemented Ability class
    
    * implemented AbilityDao interface
    
    * implemented AbilityDaoImpl class

--- haunted-houses/src/main/java/cz/muni/fi/pa165/hauntedhouses/dao/AbilityDao.java
+++ haunted-houses/src/main/java/cz/muni/fi/pa165/hauntedhouses/dao/AbilityDao.java
@@ -1,4 +1,24 @@
 package cz.muni.fi.pa165.hauntedhouses.dao;
 
+import cz.muni.fi.pa165.hauntedhouses.model.Ability;
+
+import java.util.List;
+
+/**
+ * @author David Hofman
+ */
 public interface AbilityDao {
+
+    void createAbility(Ability A);
+
+    Ability getAbilityById(Long id);
+
+    Ability getAbilityByName(String name);
+
+    List<Ability> getAllAbilities();
+
+    void updateAbility(Ability A);
+
+    void deleteAbility(Ability A);
+
 }
