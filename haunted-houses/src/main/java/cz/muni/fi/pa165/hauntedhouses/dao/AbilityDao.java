33d83e9b239f81d11d6dd49f0ad9fe9a3e545ff1
davidhofman <davidhofman1996@gmail.com>
2020-03-23 14:25:50 +0100

    added Javadoc to AbilityDao interface

--- haunted-houses/src/main/java/cz/muni/fi/pa165/hauntedhouses/dao/AbilityDao.java
+++ haunted-houses/src/main/java/cz/muni/fi/pa165/hauntedhouses/dao/AbilityDao.java
@@ -9,16 +9,43 @@ import java.util.List;
  */
 public interface AbilityDao {
 
+
+    /**
+     * Inserts an ability into the database
+     * @param A Ability to be inserted into the database
+     */
     void createAbility(Ability A);
 
+    /**
+     * Searches the database for an ability with given id
+     * @param id Id of the ability
+     * @return Ability with given id
+     */
     Ability getAbilityById(Long id);
 
+    /**
+     * Searches the database for an ability with given name
+     * @param name Name of the ability
+     * @return Ability with given name
+     */
     Ability getAbilityByName(String name);
 
+    /**
+     * Searches the database for all the abilities
+     * @return List containing all the abilities
+     */
     List<Ability> getAllAbilities();
 
+    /**
+     * Updates the given ability
+     * @param A Ability to be updated
+     */
     void updateAbility(Ability A);
 
+    /**
+     * Deletes the given ability from the database
+     * @param A Ability to be deleted
+     */
     void deleteAbility(Ability A);
 
 }
