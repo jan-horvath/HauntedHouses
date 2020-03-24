6fb3c0f68104e35458dcbf2a357bc659f6bae0ca
davidhofman <47823840+davidhofman@users.noreply.github.com>
2020-03-24 11:51:31 +0100

    Added Javadoc, updated classes. (#12)
    * added Javadoc to AbilityDao interface
    
    * updated AbilityDaoImpl class to match recent changes
    
    * changed UpdateAbility method to return the updated ability
    
    * fixed equals and hashCode methods
    
    * improved Javadoc
    
    * improved updateAbility and deleteAbility methods

--- haunted-houses/src/main/java/cz/muni/fi/pa165/hauntedhouses/dao/AbilityDao.java
+++ haunted-houses/src/main/java/cz/muni/fi/pa165/hauntedhouses/dao/AbilityDao.java
@@ -3,22 +3,52 @@ package cz.muni.fi.pa165.hauntedhouses.dao;
 import cz.muni.fi.pa165.hauntedhouses.model.Ability;
 
 import java.util.List;
+import org.springframework.orm.jpa.JpaSystemException;
 
 /**
  * @author David Hofman
  */
 public interface AbilityDao {
 
+    /**
+     * Inserts an ability into the database
+     * @param A Ability to be inserted into the database
+     * @throws JpaSystemException if the database constraints are violated or the entity already exists
+     */
     void createAbility(Ability A);
 
+    /**
+     * Searches the database for an ability with given id
+     * @param id Id of the ability
+     * @return Ability with given id, null if it doesn't exist
+     */
     Ability getAbilityById(Long id);
 
+    /**
+     * Searches the database for an ability with given name
+     * @param name Name of the ability
+     * @return Ability with given name, null if it doesn't exist
+     */
     Ability getAbilityByName(String name);
 
+    /**
+     * Searches the database for all the abilities
+     * @return List containing all the abilities
+     */
     List<Ability> getAllAbilities();
 
-    void updateAbility(Ability A);
+    /**
+     * Updates the given ability
+     * @param A Ability to be updated
+     * @return the updated ability, null if such ability doesn't exist
+     * @throws JpaSystemException if the database constraints are violated
+     */
+    Ability updateAbility(Ability A);
 
+    /**
+     * Deletes the given ability from the database if it exists
+     * @param A Ability to be deleted
+     */
     void deleteAbility(Ability A);
-
 }
+
--- haunted-houses/src/main/java/cz/muni/fi/pa165/hauntedhouses/dao/impl/AbilityDaoImpl.java
+++ haunted-houses/src/main/java/cz/muni/fi/pa165/hauntedhouses/dao/impl/AbilityDaoImpl.java
@@ -2,76 +2,55 @@ package cz.muni.fi.pa165.hauntedhouses.dao.impl;
 
 import cz.muni.fi.pa165.hauntedhouses.dao.AbilityDao;
 import cz.muni.fi.pa165.hauntedhouses.model.Ability;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.*;
+import javax.transaction.Transactional;
 import java.util.List;
 
 /**
  * @author David Hofman
  */
+@Repository
+@Transactional
 public class AbilityDaoImpl implements AbilityDao {
 
-    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+    @PersistenceContext
+    private static EntityManager em;
 
     @Override
     public void createAbility(Ability A) {
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
         em.persist(A);
-        em.getTransaction().commit();
-        em.close();
     }
 
     @Override
     public Ability getAbilityById(Long id) {
-        EntityManager em = emf.createEntityManager();
-        Ability ability = em.find(Ability.class, id);
-        em.close();
-        return ability;
+        return em.find(Ability.class, id);
     }
 
     @Override
     public Ability getAbilityByName(String name) {
-        EntityManager em = emf.createEntityManager();
-        TypedQuery<Ability> query = em.createQuery("SELECT a FROM Ability a WHERE a.name = :name", Ability.class).setParameter("name",name);
-        Ability ability;
         try {
-            ability = query.getSingleResult();
+            return em.createQuery("SELECT a FROM Ability a WHERE a.name = :name", Ability.class)
+                    .setParameter("name",name).getSingleResult();
         } catch(NoResultException e) {
-            ability = null;
-        } finally {
-            em.close();
+            return null;
         }
-        return ability;
     }
 
     @Override
     public List<Ability> getAllAbilities() {
-        EntityManager em = emf.createEntityManager();
-        List<Ability> list = em.createQuery("SELECT a FROM Ability a", Ability.class).getResultList();
-        em.close();
-        return list;
+        return em.createQuery("SELECT a FROM Ability a", Ability.class).getResultList();
     }
 
     @Override
-    public void updateAbility(Ability A) {
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        em.merge(A);
-        em.getTransaction().commit();
-        em.close();
+    public Ability updateAbility(Ability A) {
+        if(em.find(Ability.class, A.getId()) != null) return em.merge(A);
+        return null;
     }
 
     @Override
     public void deleteAbility(Ability A) {
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        if (em.contains(A)) {
-            em.remove(A);
-        } else {
-            em.remove(em.getReference(Ability.class, A.getId()));
-        }
-        em.getTransaction().commit();
-        em.close();
+        em.remove(em.contains(A) ? A : em.merge(A));
     }
 }
