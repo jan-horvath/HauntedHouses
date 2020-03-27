08de0de349e08ee6597b5451e44a19bbd65833f8
Petr Vitovsky <vitovsky.petr@gmail.com>
2020-03-27 13:06:52 +0100

    removed springbootapplication

--- haunted-houses/src/main/java/cz/muni/fi/pa165/hauntedhouses/HauntedHousesApplication.java
+++ haunted-houses/src/main/java/cz/muni/fi/pa165/hauntedhouses/HauntedHousesApplication.java
@@ -1,13 +1,14 @@
 package cz.muni.fi.pa165.hauntedhouses;
 
+import cz.muni.fi.pa165.hauntedhouses.dao.HouseDao;
+import cz.muni.fi.pa165.hauntedhouses.model.House;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
 public class HauntedHousesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HauntedHousesApplication.class, args);
+		ConfigurableApplicationContext ac = SpringApplication.run(PersistenceApplicationContext.class, args);
 	}
 
 }
