aa8a5a4328d7578e5b0de4b3d98ce2474a73e74c
Petr Vitovsky <vitovsky.petr@gmail.com>
2020-03-27 13:08:06 +0100

    removed imports

--- haunted-houses/src/main/java/cz/muni/fi/pa165/hauntedhouses/HauntedHousesApplication.java
+++ haunted-houses/src/main/java/cz/muni/fi/pa165/hauntedhouses/HauntedHousesApplication.java
@@ -1,7 +1,5 @@
 package cz.muni.fi.pa165.hauntedhouses;
 
-import cz.muni.fi.pa165.hauntedhouses.dao.HouseDao;
-import cz.muni.fi.pa165.hauntedhouses.model.House;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
