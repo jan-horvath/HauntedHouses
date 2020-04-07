fbed5f5c89dbd6626dade76b5edfcdd5e19f831a
Ján Horváth <xhorvat6@fi.muni.cz>
2020-04-07 21:51:15 +0200

    Added PlayerFacade interface in new module "haunted-houses-api"

---
+++ haunted-houses/haunted-houses-api/src/main/java/cz/muni/fi/pa165/hauntedhouses/dto/PlayerDTO.java
@@ -0,0 +1,49 @@
+package cz.muni.fi.pa165.hauntedhouses.dto;
+
+import java.util.Objects;
+
+public class PlayerDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PlayerDTO playerDTO = (PlayerDTO) o;
+        return Objects.equals(name, playerDTO.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
