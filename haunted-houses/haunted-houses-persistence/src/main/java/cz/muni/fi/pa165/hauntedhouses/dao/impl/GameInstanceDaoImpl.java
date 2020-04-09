b2968e2d65442e63d036e0286a2a4e91ca4a1723
davidhofman <davidhofman1996@gmail.com>
2020-04-09 16:22:45 +0200

    added GameInstanceDaoImpl class

---
+++ haunted-houses/haunted-houses-persistence/src/main/java/cz/muni/fi/pa165/hauntedhouses/dao/impl/GameInstanceDaoImpl.java
@@ -0,0 +1,60 @@
+package cz.muni.fi.pa165.hauntedhouses.dao.impl;
+
+import cz.muni.fi.pa165.hauntedhouses.dao.GameInstanceDao;
+import cz.muni.fi.pa165.hauntedhouses.model.GameInstance;
+import cz.muni.fi.pa165.hauntedhouses.model.Player;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+/**
+ * @author David Hofman
+ */
+@Repository
+@Transactional
+public class GameInstanceDaoImpl implements GameInstanceDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void createGameInstance(GameInstance gameInstance) {
+        em.persist(gameInstance);
+    }
+
+    @Override
+    public GameInstance getGameInstanceById(Long id) {
+        return em.find(GameInstance.class, id);
+    }
+
+    @Override
+    public GameInstance getGameInstanceByPlayer(Player player) {
+        try {
+            return em.createQuery("SELECT g FROM GameInstance g WHERE g.player = :player", GameInstance.class)
+                    .setParameter("player",player).getSingleResult();
+        } catch(NoResultException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public List<GameInstance> getAllGameInstances() {
+        return em.createQuery("SELECT g FROM GameInstance g", GameInstance.class).getResultList();
+    }
+
+    @Override
+    public GameInstance updateGameInstance(GameInstance gameInstance) {
+        if(gameInstance.getId() == null || em.find(GameInstance.class, gameInstance.getId()) == null) return null;
+        return em.merge(gameInstance);
+    }
+
+    @Override
+    public void deleteGameInstance(GameInstance gameInstance) {
+        em.remove(em.contains(gameInstance) ? gameInstance : em.merge(gameInstance));
+    }
+}
+
