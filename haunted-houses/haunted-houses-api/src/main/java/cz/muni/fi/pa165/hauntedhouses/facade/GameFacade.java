165e9f4699cec039eb0df2cc53ab8d60522b2580
Ján Horváth <xhorvat6@fi.muni.cz>
2020-04-10 10:59:45 +0200

    Adding game facade

---
+++ haunted-houses/haunted-houses-api/src/main/java/cz/muni/fi/pa165/hauntedhouses/facade/GameFacade.java
@@ -0,0 +1,8 @@
+package cz.muni.fi.pa165.hauntedhouses.facade;
+
+import cz.muni.fi.pa165.hauntedhouses.dto.BanishSpecterDTO;
+
+public interface GameFacade {
+
+    boolean banishSpecter(BanishSpecterDTO banishSpecter);
+}
