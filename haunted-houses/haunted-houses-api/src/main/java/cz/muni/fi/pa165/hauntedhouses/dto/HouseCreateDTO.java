9a2276f0c356628afb4b006eb032d3cf6e1c135f
Petr Vitovsky <vitovsky.petr@gmail.com>
2020-04-10 12:07:58 +0200

    added HouseDTO and HouseCreateDTO

---
+++ haunted-houses/haunted-houses-api/src/main/java/cz/muni/fi/pa165/hauntedhouses/dto/HouseCreateDTO.java
@@ -0,0 +1,74 @@
+package cz.muni.fi.pa165.hauntedhouses.dto;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class HouseCreateDTO {
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String address;
+
+    private LocalDate hauntedSince;
+
+    private String history;
+
+    @NotNull
+    private String hint;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getHistory() {
+        return this.history;
+    }
+
+    public void setHistory(String history) {
+        this.history = history;
+    }
+
+    public LocalDate getHauntedSince() {
+        return this.hauntedSince;
+    }
+
+    public void setHauntedSince(LocalDate hauntedSince) {
+        this.hauntedSince = hauntedSince;
+    }
+
+    public String getHint() {
+        return this.hint;
+    }
+
+    public void setHint(String hint) {
+        this.hint = hint;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof HouseDTO)) return false;
+        HouseDTO house = (HouseDTO) o;
+        return getAddress().equals(house.getAddress());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getAddress());
+    }
+}
---
+++ haunted-houses/haunted-houses-api/src/main/java/cz/muni/fi/pa165/hauntedhouses/dto/HouseDTO.java
@@ -0,0 +1,80 @@
+package cz.muni.fi.pa165.hauntedhouses.dto;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class HouseDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    private LocalDate hauntedSince;
+
+    private String history;
+
+    private String hint;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getHistory() {
+        return this.history;
+    }
+
+    public void setHistory(String history) {
+        this.history = history;
+    }
+
+    public LocalDate getHauntedSince() {
+        return this.hauntedSince;
+    }
+
+    public void setHauntedSince(LocalDate hauntedSince) {
+        this.hauntedSince = hauntedSince;
+    }
+
+    public String getHint() {
+        return this.hint;
+    }
+
+    public void setHint(String hint) {
+        this.hint = hint;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof HouseDTO)) return false;
+        HouseDTO house = (HouseDTO) o;
+        return getAddress().equals(house.getAddress());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getAddress());
+    }
+}
