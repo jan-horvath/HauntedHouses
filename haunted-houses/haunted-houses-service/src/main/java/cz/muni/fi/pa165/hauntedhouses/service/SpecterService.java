a3d4ac9af6ff8240a6160d5492933929a6ccd4ea
Ján Horváth <xhorvat6@fi.muni.cz>
2020-04-19 17:51:13 +0200

    Adding Javadoc to AbilityFacade, HouseFacade, SpecterService

--- haunted-houses/haunted-houses-service/src/main/java/cz/muni/fi/pa165/hauntedhouses/service/SpecterService.java
+++ haunted-houses/haunted-houses-service/src/main/java/cz/muni/fi/pa165/hauntedhouses/service/SpecterService.java
@@ -10,7 +10,18 @@ import org.springframework.stereotype.Service;
 @Service
 public interface SpecterService {
 
+    /**
+     * Generates random specter with random (meaningful) name, random hour of start and end of haunting, description
+     * set to "No description" and upto three random abilities from the database
+     * @return randomly generated specter
+     */
     Specter generateRandomSpecter();
 
+    /**
+     * Retrieves specter using game instance ID
+     * @param gameInstanceId
+     * @throws org.springframework.dao.DataRetrievalFailureException if such game instance does not exist
+     * @return specter associated with given game instance or null is no such specter exists
+     */
     Specter getByGameInstanceId(Long gameInstanceId);
 }
