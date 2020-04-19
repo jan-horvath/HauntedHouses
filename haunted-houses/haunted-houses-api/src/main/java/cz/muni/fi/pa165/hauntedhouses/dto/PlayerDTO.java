a02d0b056712e8adf5c19e44f0ef4ae63131f6cb
Zoltan Fridrich <445620@mail.muni.cz>
2020-04-19 19:09:20 +0200

    Player Facade and Service implemented

--- haunted-houses/haunted-houses-api/src/main/java/cz/muni/fi/pa165/hauntedhouses/dto/PlayerDTO.java
+++ haunted-houses/haunted-houses-api/src/main/java/cz/muni/fi/pa165/hauntedhouses/dto/PlayerDTO.java
@@ -10,6 +10,10 @@ public class PlayerDTO {
 
     private String email;
 
+    private String passwordHash;
+
+    private boolean admin;
+
     public Long getId() {
         return id;
     }
@@ -34,16 +38,43 @@ public class PlayerDTO {
         this.email = email;
     }
 
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PlayerDTO playerDTO = (PlayerDTO) o;
-        return Objects.equals(name, playerDTO.name);
+        return Objects.equals(email, playerDTO.email);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name);
+        return Objects.hash(email);
+    }
+
+    @Override
+    public String toString() {
+        return "PlayerDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", passwordHash='" + passwordHash + '\'' +
+                ", admin=" + admin +
+                '}';
     }
 }
