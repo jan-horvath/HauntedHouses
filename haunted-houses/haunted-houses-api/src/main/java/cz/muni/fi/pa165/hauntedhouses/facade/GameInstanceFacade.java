f5108096c745dd964584bc2bc8ab2e96dc362e34
petrv7 <vitovsky.petr@gmail.com>
2020-04-20 16:54:47 +0200

    Added javadocs, autowire SpecterFacade services through constructor, SpecterFacade test (#61)
    * added javadocs
    
    * added exceptions to javadocs
    
    * changed autowiring to constructor
    
    * added SpecterFacade test
    
    * removed unused imports

--- haunted-houses/haunted-houses-api/src/main/java/cz/muni/fi/pa165/hauntedhouses/facade/GameInstanceFacade.java
+++ haunted-houses/haunted-houses-api/src/main/java/cz/muni/fi/pa165/hauntedhouses/facade/GameInstanceFacade.java
@@ -7,10 +7,24 @@ import cz.muni.fi.pa165.hauntedhouses.dto.GameInstanceDTO;
  * @author Zoltan Fridrich
  */
 public interface GameInstanceFacade {
-
+    /**
+     * Finds the GameInstance of given Player
+     * @param playerId ID of the Player
+     * @return GameInstanceDTO of given Player if he exists and has a GameInstance, null otherwise
+     */
     GameInstanceDTO findGameInstanceByPlayerId(Long playerId);
 
+    /**
+     * Creates new GameInstance
+     * @param gameInstance DTO containing all information that is required for creating new GameInstance
+     * @throws org.springframework.dao.DataAccessException if constraints are violated
+     * @return ID of the GameInstance
+     */
     Long createGameInstance(GameInstanceCreateDTO gameInstance);
 
+    /**
+     * Deletes the GameInstance from the database
+     * @param id ID of the GameInstance
+     */
     void deleteGameInstance(Long id);
 }
--- haunted-houses/haunted-houses-service/src/main/java/cz/muni/fi/pa165/hauntedhouses/service/AbilityService.java
+++ haunted-houses/haunted-houses-service/src/main/java/cz/muni/fi/pa165/hauntedhouses/service/AbilityService.java
@@ -10,9 +10,36 @@ import java.util.List;
  */
 @Service
 public interface AbilityService {
+    /**
+     * Finds Ability with given ID in database
+     * @param id ID of the Ability
+     * @return Ability with given ID if it exists, null otherwise
+     */
     Ability getAbilityById(Long id);
+
+    /**
+     * Finds Ability with given name in database
+     * @param name Name of the Ability
+     * @return Ability with given name if it exists, null otherwise
+     */
     Ability getAbilityByName(String name);
+
+    /**
+     * Finds all Abilities in database
+     * @return List of all Abilities
+     */
     List<Ability> getAllAbilities();
+
+    /**
+     * Creates new Ability in the database
+     * @param ability New Ability
+     * @throws org.springframework.dao.DataAccessException if constraints are violated
+     */
     void createAbility(Ability ability);
+
+    /**
+     * Deletes Ability from database
+     * @param ability Ability for deletion
+     */
     void deleteAbility(Ability ability);
 }
