0e8baa2b1fb227266fd5aab85d9476fe1c88892d
433736 <61185190+433736@users.noreply.github.com>
2020-04-24 16:43:00 +0200

    Adding generic sequence generator (#81)
    * Adding game facade tests
    
    * Adding game instance facade tests
    
    * Adding generic generator for entity ID generation
    
    Co-authored-by: Ján Horváth <xhorvat6@fi.muni.cz>

--- haunted-houses/haunted-houses-persistence/src/main/java/cz/muni/fi/pa165/hauntedhouses/model/Ability.java
+++ haunted-houses/haunted-houses-persistence/src/main/java/cz/muni/fi/pa165/hauntedhouses/model/Ability.java
@@ -14,7 +14,7 @@ import java.util.Objects;
 @Entity
 public class Ability implements Serializable {
     @Id
-    @GeneratedValue(strategy=GenerationType.SEQUENCE)
+    @GeneratedValue(generator = "pooled_generator")
     private Long id;
 
     @Column(nullable=false,unique=true)
