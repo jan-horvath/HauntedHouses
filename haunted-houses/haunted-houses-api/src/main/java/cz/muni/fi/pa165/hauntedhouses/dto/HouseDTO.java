ef16d081eb76ddfcd27ee153a0e28b66fcd094f3
Petr Vitovsky <vitovsky.petr@gmail.com>
2020-05-08 14:19:57 +0200

    fixed json date format for rest api

--- haunted-houses/haunted-houses-api/src/main/java/cz/muni/fi/pa165/hauntedhouses/dto/HouseDTO.java
+++ haunted-houses/haunted-houses-api/src/main/java/cz/muni/fi/pa165/hauntedhouses/dto/HouseDTO.java
@@ -1,5 +1,6 @@
 package cz.muni.fi.pa165.hauntedhouses.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotEmpty;
@@ -20,6 +21,7 @@ public class HouseDTO {
     private String address;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern="yyyy-MM-dd")
     private Date hauntedSince;
 
     private String history;
