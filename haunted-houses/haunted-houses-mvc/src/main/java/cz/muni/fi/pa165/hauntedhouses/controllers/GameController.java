5433d887c34081b25b9bf284d6b4fc3799a058f5
Ján Horváth <xhorvat6@fi.muni.cz>
2020-05-31 15:05:33 +0200

    Fixing a bug where the app would crash when too many banishments were entered. Now, the maximum amount of banishemnts allowed is 20.

--- haunted-houses/haunted-houses-mvc/src/main/java/cz/muni/fi/pa165/hauntedhouses/controllers/GameController.java
+++ haunted-houses/haunted-houses-mvc/src/main/java/cz/muni/fi/pa165/hauntedhouses/controllers/GameController.java
@@ -1,30 +1,29 @@
 package cz.muni.fi.pa165.hauntedhouses.controllers;
 
 import cz.muni.fi.pa165.hauntedhouses.dto.*;
-import cz.muni.fi.pa165.hauntedhouses.facade.GameFacade;
-import cz.muni.fi.pa165.hauntedhouses.facade.GameInstanceFacade;
-import cz.muni.fi.pa165.hauntedhouses.facade.HouseFacade;
-import cz.muni.fi.pa165.hauntedhouses.facade.PlayerFacade;
+import cz.muni.fi.pa165.hauntedhouses.facade.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.validation.*;
 import java.security.Principal;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/game")
 public class GameController {
 
     final static Logger log = LoggerFactory.getLogger(GameController.class);
+
+    ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
 
     private GameInstanceFacade gameInstanceFacade;
 
@@ -87,18 +86,22 @@ public class GameController {
      * @return
      */
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public String create(@ModelAttribute("createDTO") GameInstanceCreateDTO createDTO,
-                         RedirectAttributes redirectAttributes, UriComponentsBuilder uriBuilder, Principal principal) {
+    public String create(@ModelAttribute("createDTO") GameInstanceCreateDTO createDTO, RedirectAttributes redirectAttributes,
+                         UriComponentsBuilder uriBuilder, Principal principal) {
         PlayerDTO foundPlayer = playerFacade.getPlayerByEmail(principal.getName());
         log.debug("\"/create\" called for user email {} (player found: {}, banishments: {})",
                 principal.getName(), foundPlayer != null, createDTO.getBanishesRequired());
 
-        if (createDTO.getBanishesRequired() <= 0) {
-            redirectAttributes.addFlashAttribute("alert_warning", "Please enter a positive number");
-            return "/game/new";
+        createDTO.setPlayer(foundPlayer);
+        Set<ConstraintViolation<@Valid GameInstanceCreateDTO>> violations =
+                validatorFactory.getValidator().validate(createDTO);
+
+        if (!violations.isEmpty()) {
+            redirectAttributes.addFlashAttribute("alert_warning",
+                    violations.iterator().next().getMessage());
+            return "redirect:" + uriBuilder.path("/game/new").toUriString();
         }
 
-        createDTO.setPlayer(foundPlayer);
         gameInstanceFacade.createGameInstance(createDTO);
         redirectAttributes.addFlashAttribute("alert_success",
                 gameInstanceFacade.getGameInstanceByPlayerId(foundPlayer.getId()));
