4292830fd03acd4ad7925486617e5486e027b4c2
Ján Horváth <xhorvat6@fi.muni.cz>
2020-06-05 19:31:55 +0200

    Adding more houses and refactoring InitialDataFacadeImpl

--- haunted-houses/haunted-houses-service/src/main/java/cz/muni/fi/pa165/hauntedhouses/service/facade/InitialDataFacadeImpl.java
+++ haunted-houses/haunted-houses-service/src/main/java/cz/muni/fi/pa165/hauntedhouses/service/facade/InitialDataFacadeImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Calendar;
+import java.util.Date;
 
 @Component
 @Transactional
@@ -20,6 +21,8 @@ public class InitialDataFacadeImpl implements InitialDataFacade {
     private PlayerService playerService;
     private HouseService houseService;
     private AbilityService abilityService;
+
+    private Calendar calendar = Calendar.getInstance();
 
     @Autowired
     public InitialDataFacadeImpl(PlayerService playerService,
@@ -37,103 +40,84 @@ public class InitialDataFacadeImpl implements InitialDataFacade {
         loadAbilities();
     }
 
+    private void createHouse(String name, String address, String history, String hint, int day, int month, int year) {
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+
+        House house = new House();
+        house.setName(name);
+        house.setAddress(address);
+        house.setHistory(history);
+        house.setHint(hint);
+        house.setHauntedSince(calendar.getTime());
+
+        houseService.createHouse(house);
+    }
+
+    private void createAbility(String name, String description) {
+        Ability ability = new Ability();
+        ability.setName(name);
+        ability.setDescription(description);
+        abilityService.createAbility(ability);
+    }
+
     private void loadHouses() {
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR, 1999);
-        cal.set(Calendar.MONTH, Calendar.NOVEMBER);
-        cal.set(Calendar.DAY_OF_MONTH, 10);
+        createHouse("Berghof", "Berchtesgaden, Bavaria, Germany",
+                "The Berghof began as a much smaller chalet called Haus Wachenfeld, " +
+                        "a holiday home built in 1916 (or 1917) by Kommerzienrat Otto Winter.",
+                "This house was Hitler's home in the Bavarian Alps", 8, Calendar.SEPTEMBER, 1918);
 
-        House house1 = new House();
-        house1.setName("Cartoon house");
-        house1.setAddress("Evergreen Terrace 742");
-        house1.setHistory("The house was built in 1989. The first haunting was recorded in 1999.");
-        house1.setHint("House of the Simpsons family.");
-        house1.setHauntedSince(cal.getTime());
-        houseService.createHouse(house1);
+        createHouse("Palace of Versailles", "Place d'Armes, 78000 Versailles, France",
+                "It was the principal royal residence of France from 1682, until the start of the " +
+                        "French Revolution in 1789", "The Sun King lived there.",
+                31, Calendar.DECEMBER, 1800);
 
-        cal.set(Calendar.YEAR, 1650);
-        cal.set(Calendar.MONTH, Calendar.JANUARY);
-        cal.set(Calendar.DAY_OF_MONTH, 5);
+        createHouse("Hagenauerhaus", "Getreidegasse 9, 5020 Salzburg, Austria",
+                "The house was built in the 12th century on ground which had been part of the garden belonging " +
+                        "to the Benedictine monks of St Peter's, Salzburg.",
+                "Mozart was born in this house.", 5, Calendar.MAY, 1995);
 
-        House house2 = new House();
-        house2.setName("Hobbit house");
-        house2.setAddress("Hobbiton, Shire");
-        house2.setHistory("Founded TA 1601.");
-        house2.setHint("House of Frodo and Bilbo Baggins.");
-        house2.setHauntedSince(cal.getTime());
-        houseService.createHouse(house2);
+        createHouse("Villa Tugendhat", "Černopolní 45, 613 00 Brno",
+                "The construction company of Artur and Mořic Eisler began construction in the summer " +
+                        "of 1929 and finished it in 14 months.",
+                "A famous building in modern architectural style designed by Ludwig Mies van der Rohe and Lilly Reich",
+                7, Calendar.AUGUST, 2017);
 
-        cal.set(Calendar.YEAR, 1739);
-        cal.set(Calendar.MONTH, Calendar.MAY);
-        cal.set(Calendar.DAY_OF_MONTH, 16);
+        createHouse("The White House", "1600 Pennsylvania Avenue NW",
+                "Built in 1792. Designed by irish architect James Hoban.",
+                "The US president lives there.", 20, Calendar.JANUARY, 2017);
 
-        House house3 = new House();
-        house3.setName("The Gingerbread House");
-        house3.setAddress("Spooky forest");
-        house3.setHistory("Built by a witch using only gingerbread and other sweets.");
-        house3.setHint("Hansel and Gretel visited this house.");
-        house3.setHauntedSince(cal.getTime());
-        houseService.createHouse(house3);
+        createHouse("Number 10", "10 Downing Street",
+                "Number 10 is over 300 years old and contains approximately 100 rooms.",
+                "The UK prime minister lives there.", 24, Calendar.JUNE, 1990);
 
-        cal.set(Calendar.YEAR, 2000);
-        cal.set(Calendar.MONTH, Calendar.DECEMBER);
-        cal.set(Calendar.DAY_OF_MONTH, 20);
+        createHouse("Empire State Building", "350 Fifth Avenue, Manhattan, NY",
+                "It was designed by Shreve, Lamb & Harmon and built from 1930 to 1931.",
+                "Its name is derived from the nickname of the city, where it stands.",
+                12, Calendar.FEBRUARY, 1999);
 
-        House house4 = new House();
-        house4.setName("The White House");
-        house4.setAddress("1600 Pennsylvania Avenue NW");
-        house4.setHistory("Built in 1792. Designed by irish architect James Hoban.");
-        house4.setHint("The US president lives there.");
-        house4.setHauntedSince(cal.getTime());
-        houseService.createHouse(house4);
+        createHouse("Leaning Tower of Pisa", "Piazza del Duomo, 56126, Pisa PI, Italy",
+                "The tower began to sink after construction had progressed to the second floor in 1178.",
+                "Torre di ...", 18, Calendar.OCTOBER, 1444);
 
-        cal.set(Calendar.YEAR, 1990);
-        cal.set(Calendar.MONTH, Calendar.JUNE);
-        cal.set(Calendar.DAY_OF_MONTH, 24);
+        createHouse("Taj Mahal", "Agra, Uttar Pradesh, India",
+                "Estimated cost of the construction is about 70 billion rupees (916 million dolars)",
+                "Lies on the southern bank of the river Yamuna", 1, 2, 1993);
 
-        House house5 = new House();
-        house5.setName("Number 10");
-        house5.setAddress("10 Downing Street");
-        house5.setHistory("Number 10 is over 300 years old and contains approximately 100 rooms.");
-        house5.setHint("The UK prime minister lives there.");
-        house5.setHauntedSince(cal.getTime());
-        houseService.createHouse(house5);
+        createHouse("Burj Khalifa", "1 Sheikh Mohammed bin Rashid Boulevard",
+                " Sheikh Khalifa, the ruler of the UAE, granted monetary aid and funding, " +
+                        "hence resulting in the changing of the name to \"Burj Khalifa\".",
+                "Tallest building in the world since 2009", 29, 9, 2013);
 
-        House house6 = new House();
-        house6.setName("Berghof");
-        house6.setAddress("Berchtesgaden, Bavaria, Germany");
-        house6.setHistory("The Berghof began as a much smaller chalet called Haus Wachenfeld, " +
-                "a holiday home built in 1916 (or 1917) by Kommerzienrat Otto Winter.");
-        house6.setHint("This house was Hitler's home in the Bavarian Alps");
-        house6.setHauntedSince(cal.getTime());
-        houseService.createHouse(house6);
+        createHouse("Potala Palace", "No. 35, Beijing Middle Road, Chengguan District, Lhasa City",
+                "This has been a museum since 1959, and is a World Heritage Site since 1994",
+                "The winter palace of the Dalai Lamas", 25, Calendar.MAY, 1646);
 
-        House house7 = new House();
-        house7.setName("Palace of Versailles");
-        house7.setAddress("Place d'Armes, 78000 Versailles, France");
-        house7.setHistory("It was the principal royal residence of France from 1682, " +
-                "until the start of the French Revolution in 1789");
-        house7.setHint("The Sun King lived there.");
-        house7.setHauntedSince(cal.getTime());
-        houseService.createHouse(house7);
-
-        House house8 = new House();
-        house8.setName("Hagenauerhaus");
-        house8.setAddress("Getreidegasse 9, 5020 Salzburg, Austria");
-        house8.setHistory("The house was built in the 12th century on ground which had been part of the garden" +
-                " belonging to the Benedictine monks of St Peter's, Salzburg.");
-        house8.setHint("Mozart was born in this house.");
-        house8.setHauntedSince(cal.getTime());
-        houseService.createHouse(house8);
-
-        House house9 = new House();
-        house9.setName("Villa Tugendhat");
-        house9.setAddress("Černopolní 45, 613 00 Brno");
-        house9.setHistory("The construction company of Artur and Mořic Eisler began construction in the summer " +
-                "of 1929 and finished it in 14 months.");
-        house9.setHint("A famous building in modern architectural style designed by Ludwig Mies van der Rohe and Lilly Reich");
-        house9.setHauntedSince(cal.getTime());
-        houseService.createHouse(house9);
+        createHouse("Taipei 101", "No. 7, Section 5, Xinyi Road, Xinyi District",
+                "In 2011 Taipei 101 received a Platinum rating under the LEED certification system to become the " +
+                        "largest green building in the world.",
+                "Tallest building between 2004 and 2010", 10, Calendar.SEPTEMBER, 2012);
     }
 
     private void loadPlayers() {
@@ -152,44 +136,13 @@ public class InitialDataFacadeImpl implements InitialDataFacade {
     }
 
     private void loadAbilities() {
-        Ability ability1 = new Ability();
-        ability1.setName("Invisibility");
-        ability1.setDescription("The specter may become invisible when nobody is watching");
-        abilityService.createAbility(ability1);
-
-        Ability ability2 = new Ability();
-        ability2.setName("Fire breathing");
-        ability2.setDescription("The specter may breathe fire.");
-        abilityService.createAbility(ability2);
-
-        Ability ability3 = new Ability();
-        ability3.setName("Summon skeletons");
-        ability3.setDescription("The specter may summons skeletons.");
-        abilityService.createAbility(ability3);
-
-        Ability ability4 = new Ability();
-        ability4.setName("Hide under bed");
-        ability4.setDescription("The specter might be hiding under your bed IRL.");
-        abilityService.createAbility(ability4);
-
-        Ability ability5 = new Ability();
-        ability5.setName("Protection");
-        ability5.setDescription("The specter will haunt you for the rest of your life if we get a low score for this milestone.");
-        abilityService.createAbility(ability5);
-
-        Ability ability6 = new Ability();
-        ability6.setName("Time travel");
-        ability6.setDescription("The specter can travel through time but only into the future at a rate of one second per second");
-        abilityService.createAbility(ability6);
-
-        Ability ability7 = new Ability();
-        ability7.setName("Insect transformation");
-        ability7.setDescription("The specter can transform into any insect... Irreversibly.");
-        abilityService.createAbility(ability7);
-
-        Ability ability8 = new Ability();
-        ability8.setName("Poisonous clouds");
-        ability8.setDescription("The specter can create highly poisonous clouds around it. The specter is not immune to the poison.");
-        abilityService.createAbility(ability8);
+        createAbility("Invisibility", "The specter may become invisible when nobody is watching");
+        createAbility("Fire breathing", "The specter may breathe fire.");
+        createAbility("Summon skeletons", "The specter may summons skeletons.");
+        createAbility("Hide under bed", "The specter might be hiding under your bed IRL.");
+        createAbility("Protection", "The specter will haunt you for the rest of your life if we get a low score for this milestone.");
+        createAbility("Time travel", "The specter can travel through time but only into the future at a rate of one second per second");
+        createAbility("Insect transformation","The specter can transform into any insect... Irreversibly.");
+        createAbility("Poisonous clouds", "The specter can create highly poisonous clouds around it. The specter is not immune to the poison.");
     }
 }
